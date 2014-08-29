7d85e8e926d30b566e9e32257ca65131d95c2216
Robert Castelo <robert.castelo@upf.edu>
2014-08-29 21:08:49 +0000

    Replace argument eltHits by elt.Hits in the call to mapCoords()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantFiltering@93703 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/commonFunctions.R
+++ R/commonFunctions.R
@@ -471,7 +471,7 @@ adjustForStrandSense <- function(variantsGR, alleles){
     stop("cDNApos: metadata column 'TXID' not found in input GRanges object.")
 
   exonsbytx <- exonsBy(txdb, by="tx")
-  map <- mapCoords(gr, exonsbytx, eltHits=TRUE)
+  map <- mapCoords(gr, exonsbytx, elt.Hits=TRUE)
   eolap <- map$eltHits
   qolap <- map$queryHits
   txids <- rep(names(exonsbytx), elementLengths(exonsbytx))[eolap]
