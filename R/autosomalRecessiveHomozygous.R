9c0e5b5fce788bf9fd573fc7f115d1657334dda8
r.castelo <r.castelo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-01-27 17:12:05 +0000

    Refactored autosomalRecessiveHomozygous() to allow iterating through the VCF file in chunks using the TabixFile representation.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantFiltering@98753 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/autosomalRecessiveHomozygous.R
+++ R/autosomalRecessiveHomozygous.R
@@ -3,9 +3,9 @@ setMethod("autosomalRecessiveHomozygous", signature(param="VariantFilteringParam
 
   callobj <- match.call()
   callstr <- deparse(callobj)
-  input_list <- as.list(path(param$vcfFiles))
+  vcfFiles <- param$vcfFiles
   ped <- param$pedFilename
-  genomeInfo <- param$seqInfos[[1]]
+  seqInfos <- param$seqInfos
   txdb <- param$txdb
   bsgenome <- param$bsgenome
   filterTag <- param$filterTag
@@ -13,112 +13,60 @@ setMethod("autosomalRecessiveHomozygous", signature(param="VariantFilteringParam
   if (!exists(as.character(substitute(BPPARAM))))
     stop(sprintf("Parallel back-end function %s given in argument 'BPPARAM' does not exist in the current workspace. Either you did not write correctly the function name or you did not load the package 'BiocParallel'.", as.character(substitute(BPPARAM))))
   
-  if (length(input_list) > 1) {
-    multiSample <- FALSE
-  } else if (length(input_list) == 1) {
-    multiSample <- TRUE
-  } else {
+  if (length(vcfFiles) > 1) {
+    stop("More than one input VCF file is currently not supported. Please either merge the VCF files into a single one with vcftools, do the variant calling simultaneously on all samples, or proceed analysing each file separately.")
+  } else if (length(vcfFiles) < 1)
     stop("A minimum of 1 vcf file has to be provided")
-  }
   
-  pedf <- read.table(ped, header=F)
+  pedf <- read.table(ped, header=FALSE)
+
+  if (sum(pedf[, 6] == 2) < 1)
+    stop("No affected individuals detected. Something is wrong with the PED file.")
   
   unaff <- pedf[pedf[, 6] == 1, ]
-  unaff_ind <- as.character(unaff[, 2])
-
   aff <- pedf[pedf[, 6] == 2, ]
-  aff_ind <- as.character(aff[, 2])
   
-  vcf_vcf1 <- NULL
-  if (multiSample) {
-    message("Reading input VCF file into main memory.")
-    vcf_vcf1 <- readVcf(unlist(input_list), genomeInfo)
-    
-    carriers <- switch(nrow(unaff),
-                       one_ind_ms(vcf_vcf1, "0/1", unaff, filterTag),
-                       two_ind_ms(vcf_vcf1, "0/1", unaff, filterTag),
-                       three_ind_ms(vcf_vcf1, "0/1", unaff, filterTag),
-                       four_ind_ms(vcf_vcf1, "0/1", unaff, filterTag),
-                       five_ind_ms(vcf_vcf1, "0/1", unaff, filterTag))
-    
-    affected <- switch(nrow(aff)+1,
-                       stop("No affected individuals detected. Something might be wrong with the .ped file..."),
-                       one_ind_ms(vcf_vcf1, "1/1", aff, filterTag),
-                       two_ind_ms(vcf_vcf1, "1/1", aff, filterTag),
-                       three_ind_ms(vcf_vcf1, "1/1", aff, filterTag),
-                       four_ind_ms(vcf_vcf1, "1/1", aff, filterTag),
-                       five_ind_ms(vcf_vcf1, "1/1", aff, filterTag))
-  } else {
+  annotated_variants <- GRanges()
+  open(vcfFiles[[1]])
+  while (nrow(vcf <- readVcf(vcfFiles[[1]], genome=seqInfos[[1]]))) {
+    message(sprintf("%d variants read from input VCF file into main memory.", nrow(vcf)))
    
-    ## by now we have to disable this
-    stop("More than one unique sample VCF files is currently unsupported. Please do the variant call with all the individuals altogether or proceed analysing each file separately")
-
-    input_list_ind <- sapply(input_list, function(x) gsub("\\.vcf$", "", gsub("\\.vcf\\.gz$", "", x, ignore.case=TRUE), ignore.case=TRUE))
-    
-    input_list_unaff_vector <- c()
-    input_list_aff_vector <- c()
-      
-      for (i in 1:length(input_list_ind)) {
-        if (input_list_ind[i] %in% unaff[, 2]) {
-          input_list_unaff_vector <- c(input_list_unaff_vector, i)
-        } else if (input_list_ind[i]%in% aff[, 2]) {
-          input_list_aff_vector <- c(input_list_aff_vector, i)
-      }
+    carriers <- switch(nrow(unaff),
+                       one_ind_ms(vcf, "0/1", unaff, filterTag),
+                       two_ind_ms(vcf, "0/1", unaff, filterTag),
+                       three_ind_ms(vcf, "0/1", unaff, filterTag),
+                       four_ind_ms(vcf, "0/1", unaff, filterTag),
+                       five_ind_ms(vcf, "0/1", unaff, filterTag))
+  
+    affected <- switch(nrow(aff)+1,
+                       stop("No affected individuals detected. Something is wrong with the PED file."),
+                       one_ind_ms(vcf, "1/1", aff, filterTag),
+                       two_ind_ms(vcf, "1/1", aff, filterTag),
+                       three_ind_ms(vcf, "1/1", aff, filterTag),
+                       four_ind_ms(vcf, "1/1", aff, filterTag),
+                       five_ind_ms(vcf, "1/1", aff, filterTag))
+  
+    variants <- affected
+    if (length(carriers) >= 1) {
+      variants <- affected[sharedVariants(affected, carriers)]
     }
   
-    input_list_unaff <- input_list[input_list_unaff_vector]
-    input_list_aff <- input_list[input_list_aff_vector]
-    
-    carriers <- switch(length(input_list_unaff),
-                       one_ind_us(input_list_unaff, "0/1", filterTag, genomeInfo),
-                       two_ind_us(input_list_unaff, "0/1", filterTag, genomeInfo),
-                       three_ind_us(input_list_unaff, "0/1", filterTag, genomeInfo),
-                       four_ind_us(input_list_unaff, "0/1", filterTag, genomeInfo),
-                       five_ind_us(input_list_unaff, "0/1", filterTag, genomeInfo))
-    
-    affected <- switch(length(input_list_aff)+1,
-                       stop("No affected individuals detected. Something might be wrong with the .ped file..."),
-                       one_ind_us(input_list_aff, "1/1", filterTag, genomeInfo),
-                       two_ind_us(input_list_aff, "1/1", filterTag, genomeInfo),
-                       three_ind_us(input_list_aff, "1/1", filterTag, genomeInfo),
-                       four_ind_us(input_list_aff, "1/1", filterTag, genomeInfo),
-                       five_ind_us(input_list_aff, "1/1", filterTag, genomeInfo))
-    
+    variants <- .matchSeqinfo(variants, txdb, bsgenome)
+
+    annotated_variants <- c(annotated_variants, annotationEngine(variants, param, BPPARAM=BPPARAM))
   }
-  
-  ### shared variants
-  
-  ### now lets select only those which fulfill the inheritance model
-  
-  recessive <- affected
-  if (length(carriers) >= 1) {
-    realcommonrecessive <- sharedVariants(affected, carriers)
-    recessive <- affected[realcommonrecessive]
+  close(vcfFiles[[1]])
+
+  MAFpopMask <- NA
+  if ("MafDb" %in% sapply(param$otherAnnotations, class)) {
+    cnAF <- colnames(mcols(annotated_variants))
+    cnAF <- cnAF[grep("AF", cnAF)]
+    MAFpopMask <- rep(TRUE, length(cnAF))
+    names(MAFpopMask) <- cnAF
   }
-  
-  recessive <- .matchSeqinfo(recessive, txdb, bsgenome)
-
-  ##########################
-  ##                      ##
-  ##      ANNOTATION      ##
-  ##                      ##
-  ##########################
-  
-  recessive_annotated <- annotationEngine(recessive, param, BPPARAM=BPPARAM)
-
-  ##########################
-  ##                      ##
-  ## BUILD RESULTS OBJECT ##
-  ##                      ##
-  ##########################
-
-  cnAF <- colnames(mcols(recessive_annotated))
-  cnAF <- cnAF[grep("AF", cnAF)]
-  MAFpopMask <- rep(TRUE, length(cnAF))
-  names(MAFpopMask) <- cnAF
 
   new("VariantFilteringResults", callObj=callobj, callStr=callstr, inputParameters=param,
-      inheritanceModel="autosomal recessive homozygous", variants=recessive_annotated,
+      inheritanceModel="autosomal recessive homozygous", variants=annotated_variants,
       dbSNPflag=NA_character_, OMIMflag=NA_character_, variantType="Any",
       aaChangeType="Any", MAFpopMask=MAFpopMask, naMAF=TRUE, maxMAF=1,
       minPhastCons=NA_real_, minPhylostratumIndex=NA_integer_,
