38a828c54b070652d18c09956d7cd45061d8a249
Robert Castelo <robert.castelo@upf.edu>
2015-01-29 08:26:10 +0000

    Refactored unrelatedIndividuals() to allow iterating through the VCF file in chunks using the TabixFile representation.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantFiltering@98841 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/unrelatedIndividuals.R
+++ R/unrelatedIndividuals.R
@@ -3,78 +3,82 @@ setMethod("unrelatedIndividuals", signature(param="VariantFilteringParam"),
 
   callobj <- match.call()
   callstr <- deparse(callobj)
-  input_list <- as.list(path(param$vcfFiles))
-  genomeInfo <- param$seqInfos[[1]]
+  vcfFiles <- path(param$vcfFiles)
+  seqInfos <- param$seqInfos
   txdb <- param$txdb
   bsgenome <- param$bsgenome
   
   if (!exists(as.character(substitute(BPPARAM))))
     stop(sprintf("Parallel back-end function %s given in argument 'BPPARAM' does not exist in the current workspace. Either you did not write correctly the function name or you did not load the package 'BiocParallel'.", as.character(substitute(BPPARAM))))
   
-  if (class(txdb) != "TxDb")
-    stop("argument 'txdb' should be a 'TxDb' object (see GenomicFeatures package)\n")
+  if (length(vcfFiles) > 1) {
+    stop("More than one input VCF file is currently not supported. Please either merge the VCF files into a single one with vcftools, do the variant calling simultaneously on all samples, or proceed analysing each file separately.")
+  } else if (length(vcfFiles) < 1)
+    stop("A minimum of 1 vcf file has to be provided")
+
   
-  message("Reading input VCF file into main memory.")
-  vcf1 <- readVcf(unlist(input_list), genomeInfo)
-  unrelated <- rowData(vcf1)
+  annotated_variants <- GRanges()
+  open(vcfFiles[[1]])
+  n.var <- 0
+  while(nrow(vcf <- readVcf(vcfFiles[[1]], genome=seqInfos[[1]]))) {
 
-  unrelated <- .matchSeqinfo(unrelated, txdb, bsgenome)
+    variants <- rowData(vcf)
 
-  ##########################
-  ##                      ##
-  ##      ANNOTATION      ##
-  ##                      ##
-  ##########################
-  
-  unrelated_annotated <- annotationEngine(unrelated, param, BPPARAM=BPPARAM)
+    variants <- .matchSeqinfo(variants, txdb, bsgenome)
 
-  homalt <- geno(vcf1)$GT == "1/1" | geno(vcf1)$GT == "1|1"
-  colnames(homalt) <- paste0("homALT_", colnames(homalt))
-  homalt <- homalt[unrelated_annotated$IDX, , drop=FALSE]
-  rownames(homalt) <- NULL
-  mcols(unrelated_annotated) <- cbind(mcols(unrelated_annotated), DataFrame(homalt))
-  homalt <- DataFrame(HomozygousAlt=CharacterList(apply(homalt, 1,
-                                                        function(mask, snames)
-                                                          snames[mask],
-                                                        rownames(colData(vcf1)))))
-  mcols(unrelated_annotated) <- cbind(mcols(unrelated_annotated), DataFrame(homalt))
+    variants <- annotationEngine(variants, param, BPPARAM=BPPARAM)
 
-  het <- geno(vcf1)$GT == "0/1" | geno(vcf1)$GT == "0|1" |
-         geno(vcf1)$GT == "1/0" | geno(vcf1)$GT == "1|0"
-  colnames(het) <- paste0("het_", colnames(het))
-  het <- het[unrelated_annotated$IDX, , drop=FALSE]
-  rownames(het) <- NULL
-  mcols(unrelated_annotated) <- cbind(mcols(unrelated_annotated), DataFrame(het))
-  het <- DataFrame(Heterozygous=CharacterList(apply(het, 1,
-                                                    function(mask, snames)
-                                                       snames[mask],
-                                                     rownames(colData(vcf1)))))
-  mcols(unrelated_annotated) <- cbind(mcols(unrelated_annotated), DataFrame(het))
+    homalt <- geno(vcf)$GT == "1/1" | geno(vcf)$GT == "1|1"
+    colnames(homalt) <- paste0("homALT_", colnames(homalt))
+    homalt <- homalt[variants$IDX, , drop=FALSE]
+    rownames(homalt) <- NULL
+    mcols(variants) <- cbind(mcols(variants), DataFrame(homalt))
+    homalt <- DataFrame(HomozygousAlt=CharacterList(apply(homalt, 1,
+                                                          function(mask, snames)
+                                                            snames[mask],
+                                                          rownames(colData(vcf)))))
+    mcols(variants) <- cbind(mcols(variants), DataFrame(homalt))
 
-  homref <- geno(vcf1)$GT == "0/0" | geno(vcf1)$GT == "0|0"
-  colnames(homref) <- paste0("homREF_", colnames(homref))
-  homref <- homref[unrelated_annotated$IDX, , drop=FALSE]
-  rownames(homref) <- NULL
-  mcols(unrelated_annotated) <- cbind(mcols(unrelated_annotated), DataFrame(homref))
-  homref <- DataFrame(HomozygousRef=CharacterList(apply(homref, 1,
-                                                        function(mask, snames)
-                                                          snames[mask],
-                                                        rownames(colData(vcf1)))))
-  mcols(unrelated_annotated) <- cbind(mcols(unrelated_annotated), DataFrame(homref))
+    het <- geno(vcf)$GT == "0/1" | geno(vcf)$GT == "0|1" |
+           geno(vcf)$GT == "1/0" | geno(vcf)$GT == "1|0"
+    colnames(het) <- paste0("het_", colnames(het))
+    het <- het[variants$IDX, , drop=FALSE]
+    rownames(het) <- NULL
+    mcols(variants) <- cbind(mcols(variants), DataFrame(het))
+    het <- DataFrame(Heterozygous=CharacterList(apply(het, 1,
+                                                      function(mask, snames)
+                                                         snames[mask],
+                                                       rownames(colData(vcf)))))
+    mcols(variants) <- cbind(mcols(variants), DataFrame(het))
 
-  ##########################
-  ##                      ##
-  ## BUILD RESULTS OBJECT ##
-  ##                      ##
-  ##########################
+    homref <- geno(vcf)$GT == "0/0" | geno(vcf)$GT == "0|0"
+    colnames(homref) <- paste0("homREF_", colnames(homref))
+    homref <- homref[variants$IDX, , drop=FALSE]
+    rownames(homref) <- NULL
+    mcols(variants) <- cbind(mcols(variants), DataFrame(homref))
+    homref <- DataFrame(HomozygousRef=CharacterList(apply(homref, 1,
+                                                          function(mask, snames)
+                                                            snames[mask],
+                                                          rownames(colData(vcf)))))
+    mcols(variants) <- cbind(mcols(variants), DataFrame(homref))
 
-  cnAF <- colnames(mcols(unrelated_annotated))
-  cnAF <- cnAF[grep("AF", cnAF)]
-  MAFpopMask <- rep(TRUE, length(cnAF))
-  names(MAFpopMask) <- cnAF
+    annotated_variants <- c(annotated_variants, variants)
+
+    n.var <- n.var + nrow(vcf)
+    message(sprintf("%d variants processed", n.var))
+  }
+  close(vcfFiles[[1]])
+
+  MAFpopMask <- NA
+  if ("MafDb" %in% sapply(param$otherAnnotations, class)) {
+    cnAF <- colnames(mcols(annotated_variants))
+    cnAF <- cnAF[grep("AF", cnAF)]
+    MAFpopMask <- rep(TRUE, length(cnAF))
+    names(MAFpopMask) <- cnAF
+  }
 
   new("VariantFilteringResultsUI", callObj=callobj, callStr=callstr, inputParameters=param,
-      inheritanceModel="unrelated individuals", variants=unrelated_annotated,
+      inheritanceModel="unrelated individuals", variants=annotated_variants,
       indselected=NA_character_, selectgene=NA_character_, dbSNPflag=NA_character_, OMIMflag=NA_character_,
       variantType="Any", aaChangeType="Any", MAFpopMask=MAFpopMask, naMAF=TRUE, maxMAF=1,
       minPhastCons=NA_real_, minPhylostratumIndex=NA_integer_,
