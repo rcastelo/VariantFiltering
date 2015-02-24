edcf98177eb30272ead397a22f033b1d6ff7d06a
Robert Castelo <robert.castelo@upf.edu>
2015-02-24 08:25:32 +0000

    Updated autosomalDominant() and xLinked() to enable streaming over the input VCF file.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantFiltering@99738 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xLinked.R
+++ R/xLinked.R
@@ -1,151 +1,95 @@
 setMethod("xLinked", signature(param="VariantFilteringParam"),
           function(param, BPPARAM=bpparam()) {
 
+  ## store call for reproducing it later
   callobj <- match.call()
   callstr <- deparse(callobj)
-  input_list <- as.list(path(param$vcfFiles))
+
+  ## fetch necessary parameters
+  vcfFiles <- param$vcfFiles
   ped <- param$pedFilename
-  sinfo <- param$seqInfos[[1]]
-  orgdb <- param$orgdb
+  seqInfos <- param$seqInfos
   txdb <- param$txdb
-  snpdb <- param$snpdb
   bsgenome <- param$bsgenome
-  radicalAAchangeMatrix <- param$radicalAAchangeMatrix
-  allTranscripts <- param$allTranscripts
-  otherAnnotations <- param$otherAnnotations
-  filterTag <- param$filterTag
-  
-  genomeInfo <- sinfo
   
   if (!exists(as.character(substitute(BPPARAM))))
     stop(sprintf("Parallel back-end function %s given in argument 'BPPARAM' does not exist in the current workspace. Either you did not write correctly the function name or you did not load the package 'BiocParallel'.", as.character(substitute(BPPARAM))))
   
-  if (class(txdb) != "TxDb")
-    stop("argument 'txdb' should be a 'TxDb' object (see GenomicFeatures package)\n")
- 
-  if (length(input_list) > 1) {
-    multiSample <- FALSE
-  } else if (length(input_list) == 1) {
-    multiSample <- TRUE
-  } else {
-    stop("A minimum of 1 vcf file have to be provided")
+  if (length(vcfFiles) > 1)
+    stop("More than one input VCF file is currently not supported. Please either merge the VCF files into a single one with software such as vcftools or GATK, or do the variant calling simultaneously on all samples, or proceed analyzing each file separately.")
+  else if (length(vcfFiles) < 1)
+    stop("A minimum of 1 vcf file has to be provided")
+  
+  pedf <- read.table(ped, header=FALSE, stringsAsFactors=FALSE)
+  pedf <- pedf[, 1:6]
+  colnames(pedf) <- c("FamilyID", "IndividualID", "FatherID", "MotherID", "Gender", "Phenotype")
+  
+  ## assuming Phenotype == 2 means affected and Phenotype == 1 means unaffected
+  if (sum(pedf$Phenotype  == 2) < 1)
+    stop("No affected individuals detected in PED file.")
+
+  unaff_males <- pedf[pedf$Phenotype == 1 && pedf$Gender == 1, ]
+  carrier_females <-  pedf[pedf$Phenotype == 1 && pedf$Gender == 2, ]
+  
+  aff_males <- pedf[pedf$Phenotype == 2 && pedf$Gender == 1, ]
+
+  if (nrow(aff_males) < 1)
+    stop("Current 'X-linked' analysis requires at least one affected male.")
+  
+  annotated_variants <- VRanges()
+  open(vcfFiles[[1]])
+  n.var <- 0
+  while (nrow(vcf <- readVcf(vcfFiles[[1]], genome=seqInfos[[1]]))) {
+
+    ## insert an index for each variant in the VCF file
+    info(header(vcf)) <- rbind(info(header(vcf)),
+                               DataFrame(Number=1, Type="Integer",
+                                         Description="Variant index in the VCF file.",
+                                         row.names="VCFIDX"))
+    info(vcf)$VCFIDX <- (n.var+1):(n.var+nrow(vcf))
+    varIDs <- names(rowData(vcf))
+
+    n.var <- n.var + nrow(vcf)
+
+    ## restrict variants upfront to those in chromosome X
+    vcf <- vcf[seqnames(vcf) %in% c("chrX", "X"), ]
+
+    ## build logical mask of carrier females, unaffected males and affected males
+    ## variants in carrier females should be heterozygous, in unaffected males
+    ## should be homozygous reference and in affected males homozygous alternative
+    carrierFemalesMask <- unaffectedMalesMask <- rep(TRUE, nrow(vcf))
+
+    if (nrow(carrier_females) > 0) {
+      carrierFemalesMask <- geno(vcf)$GT[, carrier_females$IndividualID, drop=FALSE] == "0/1"
+      carrierFemalesMask <- apply(carrierFemalesMask, 1, all)
+    }
+
+    if (nrow(unaff_males) > 0) {
+      unaffectedMalesMask <- geno(vcf)$GT[, unaff_males$IndividualID, drop=FALSE] == "0/0"
+      unaffectedMalesMask <- apply(unaffectedMalesMask, 1, all)
+    }
+
+    affectedMalesMask <- geno(vcf)$GT[, aff_males$IndividualID, drop=FALSE] == "1/1"
+    affectedMalesMask <- apply(affectedMalesMask, 1, all)
+
+    ## filter out variants that do not segregate as an 'X-linked' trait
+    vcf <- vcf[carrierFemalesMask & unaffectedMalesMask & affectedMalesMask, ]
+
+    ## coerce the VCF object to a VRanges object
+    variants <- as(vcf, "VRanges")
+
+    ## since the conversion of VCF to VRanges strips the VCF ID field, let's put it back
+    variants$VARID <- varIDs[variants$VCFIDX]
+
+    ## harmonize Seqinfo data between variants, annotations and reference genome
+    variants <- .matchSeqinfo(variants, txdb, bsgenome)
+  
+    ## annotate variants
+    annotated_variants <- c(annotated_variants, annotationEngine(variants, param, BPPARAM=BPPARAM))
+
+    message(sprintf("%d variants processed", n.var))
   }
-  
-  pedf <- read.table(ped, header=F)
-  
-  unaff_males <- pedf[pedf[, 6] == 1 & pedf[, 5] == 1, ]
-  carrier_female <-  pedf[pedf[, 6] == 1 & pedf[, 5] == 2, ]
-  
-  aff_males <- pedf[pedf[, 6] == 2 & pedf[, 5] == 1, ]
-  
-  
-  if (multiSample) {
-    message("Reading input VCF file into main memory.")
-    vcf_vcf1 <- readVcf(unlist(input_list), genomeInfo) 
-    
-    carrierFemales <- switch (nrow(carrier_female),
-                              one_ind_ms(vcf_vcf1, "0/1", carrier_female, filterTag),
-                              two_ind_ms(vcf_vcf1, "0/1", carrier_female, filterTag),
-                              three_ind_ms(vcf_vcf1, "0/1", carrier_female, filterTag),
-                              four_ind_ms(vcf_vcf1, "0/1", carrier_female, filterTag),
-                              five_ind_ms(vcf_vcf1, "0/1", carrier_female, filterTag))
-    
-    unaff_males <- switch (nrow(unaff_males),
-                          one_ind_ms(vcf_vcf1, "0/0", unaff_males, filterTag),
-                          two_ind_ms(vcf_vcf1, "0/0", unaff_males, filterTag),
-                          three_ind_ms(vcf_vcf1, "0/0", unaff_males, filterTag),
-                          four_ind_ms(vcf_vcf1, "0/0", unaff_males, filterTag),
-                          five_ind_ms(vcf_vcf1, "0/0", unaff_males, filterTag))
-    
-    affected <- switch (nrow(aff_males)+1,
-                        stop("No affected males detected. Something might be wrong with the .ped file..."),
-                        one_ind_ms(vcf_vcf1, "1/1", aff_males, filterTag),
-                        two_ind_ms(vcf_vcf1, "1/1", aff_males, filterTag),
-                        three_ind_ms(vcf_vcf1, "1/1", aff_males, filterTag),
-                        four_ind_ms(vcf_vcf1, "1/1", aff_males, filterTag),
-                        five_ind_ms(vcf_vcf1, "1/1", aff_males, filterTag))
-
-    
-    if (length(unaff_males) < 1) {
-      affected_filtered <- affected
-    } else {
-      
-      realaffected <- sharedVariants(affected, unaff_males)
-      affected_filtered <- affected[realaffected]
-    }
-    
-  } else {
-    
-    input_list_ind <- sapply(input_list, function(x) gsub("\\.vcf$", "", gsub("\\.vcf\\.gz$", "", x, ignore.case=TRUE), ignore.case=TRUE))
-    
-    input_list_unaff_males_vector <- c()
-    input_list_carrier_female_vector <- c()
-    input_list_aff_males_vector <- c()
-    
-    for (i in 1:length(input_list_ind)) {
-      if (input_list_ind[i] %in% unaff_males[, 2]) {
-        input_list_unaff_males_vector <-  c(input_list_unaff_males_vector, i)
-      } else if (input_list_ind[i]%in% carrier_female[, 2]) {
-        input_list_carrier_female_vector <-  c(input_list_carrier_female_vector, i)
-      } else if (input_list_ind[i]%in% aff_males[, 2]) {
-        input_list_aff_males_vector <- c(input_list_aff_males_vector, i)
-      }
-    }
-    
-    input_list_unaff_males <- input_list[input_list_unaff_males_vector]
-    input_list_carrier_female <- input_list[input_list_carrier_female_vector]
-    input_list_aff_males <- input_list[input_list_aff_males_vector]
-    
-    carrierFemales <- switch (length(input_list_carrier_female),
-                              one_ind_us(input_list_carrier_female, "0/1", filterTag, genomeInfo),
-                              two_ind_us(input_list_carrier_female, "0/1", filterTag, genomeInfo),
-                              three_ind_us(input_list_carrier_female, "0/1", filterTag, genomeInfo),
-                              four_ind_us(input_list_carrier_female, "0/1", filterTag, genomeInfo),
-                              five_ind_us(input_list_carrier_female, "0/1", filterTag, genomeInfo))
-    
-    affected <- switch (length(input_list_aff_males)+1,
-                        stop("No affected males detected. Something might be wrong with the .ped file..."),
-                        one_ind_us(input_list_aff_males, "1/1", filterTag, genomeInfo),
-                        two_ind_us(input_list_aff_males, "1/1", filterTag, genomeInfo),
-                        three_ind_us(input_list_aff_males, "1/1", filterTag, genomeInfo),
-                        four_ind_us(input_list_aff_males, "1/1", filterTag, genomeInfo),
-                        five_ind_us(input_list_aff_males, "1/1", filterTag, genomeInfo))
-    
-    if (length(input_list_unaff_males) < 1) {
-      affected_filtered <- affected
-    } else {
-      affected_filtered <- pullout_shared_us(affected, input_list_unaff_males, filterTag, genomeInfo)
-    }
-  }   
-  
-  if (length(carrierFemales) < 1) {
-    xl_all <- affected_filtered
-  } else {
-    realcommonXlinked <- sharedVariants(affected_filtered, carrierFemales)
-    xl_all <- affected_filtered[realcommonXlinked]
-  }
-  
-  xl_allchr <- .matchSeqinfo(xl_all, txdb, bsgenome)
-  
-  Xlist <- which(seqnames(xl_allchr) == "chrX")
-  xlinked <- xl_allchr[Xlist]
-
-  ##########################
-  ##                      ##
-  ##      ANNOTATION      ##
-  ##                      ##
-  ##########################
-  
-  xlinked_annotated <- annotationEngine(xlinked, orgdb=orgdb, txdb=txdb, snpdb=snpdb,
-                                        radicalAAchangeMatrix=radicalAAchangeMatrix,
-                                        otherAnnotations=otherAnnotations,
-                                        allTranscripts=allTranscripts, BPPARAM=BPPARAM)
-
-  ##########################
-  ##                      ##
-  ## BUILD RESULTS OBJECT ##
-  ##                      ##
-  ##########################
+  close(vcfFiles[[1]])
 
   locMask <- do.call("names<-", list(rep(TRUE, nlevels(annotated_variants$LOCATION)),
                                      levels(annotated_variants$LOCATION)))
