6ae4a798f1bc505ffff09dc92e67824752d41171
r.castelo <r.castelo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-15 23:11:57 +0000

    Hack to fix the timeout building in windows.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/VariantFiltering@117522 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllUtilities.R
+++ R/AllUtilities.R
@@ -141,3 +141,22 @@
 
   pedDf
 }
+
+## get the operating system
+## adapted from http://www.r-bloggers.com/identifying-the-os-from-r/
+.getOS <- function() {
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)) {
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else {
+    ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
