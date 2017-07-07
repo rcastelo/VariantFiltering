5a5be35e7a8eb18341e1f3b6a30dafd50697d587
Robert Castelo <robert.castelo@upf.edu>
2017-07-07 13:16:04 +0200

    Setting environment of functions in FilterRules objects to baseenv to minimize memory requirements when serializing this kind of object.

--- R/defaultFilters.R
+++ R/defaultFilters.R
@@ -1,30 +1,30 @@
-.defaultFilters <- list(dbSNP=function(x) !is.na(allVariants(x, groupBy="nothing")$dbSNP),
-                        OMIM=function(x) !is.na(allVariants(x, groupBy="nothing")$OMIM),
-                        variantType=function(x) allVariants(x, groupBy="nothing")$TYPE %in% names(cutoffs(x)$variantType)[cutoffs(x)$variantType],
+.defaultFilters <- list(dbSNP=function(x) !is.na(VariantFiltering::allVariants(x, groupBy="nothing")$dbSNP),
+                        OMIM=function(x) !is.na(VariantFiltering::allVariants(x, groupBy="nothing")$OMIM),
+                        variantType=function(x) VariantFiltering::allVariants(x, groupBy="nothing")$TYPE %in% names(VariantFiltering::cutoffs(x)$variantType)[VariantFiltering::cutoffs(x)$variantType],
                         aaChangeType=function(x) {
                           mask <- rep(TRUE, length(x))
-                          if (cutoffs(x)$aaChangeType %in% c("Conservative", "Radical")) {
-                            aachangetype <- allVariants(x, groupBy="nothing")$AAchangeType
-                            mask <- is.na(aachangetype) | achangetype %in% cutoffs(x)$aaChangeType
+                          if (VariantFiltering::cutoffs(x)$aaChangeType %in% c("Conservative", "Radical")) {
+                            aachangetype <- VariantFiltering::allVariants(x, groupBy="nothing")$AAchangeType
+                            mask <- is.na(aachangetype) | achangetype %in% VariantFiltering::cutoffs(x)$aaChangeType
                           }
                           mask
                         },
                         SOterms=function(x) {
-                          soterms <- cutoffs(x)$SOterms
+                          soterms <- VariantFiltering::cutoffs(x)$SOterms
                           if (is.null(soterms) || all(is.na(soterms))) ## no SO terms specified imply no restriction
-                            soterms <- nodes(sog(x))[sapply(nodeData(sog(x), nodes(sog(x)), "varIdx"), length) > 0]
+                            soterms <- graph::nodes(VariantFiltering::sog(x))[sapply(graph::nodeData(VariantFiltering::sog(x), graph::nodes(VariantFiltering::sog(x)), "varIdx"), length) > 0]
                           else {
-                            soterms <- .findSOIDs(cutoffs(x)$SOterms, sog(x))
+                            soterms <- VariantFiltering:::.findSOIDs(VariantFiltering::cutoffs(x)$SOterms, VariantFiltering::sog(x))
 
                             ## restrict to the given SO terms and all their more specific ancestors with 
                             ## at least one annotated variant
-                            soterms <- unique(c(soterms, .ancestorsSO(param(x), soterms)))
-                            soterms <- soterms[sapply(nodeData(sog(x), soterms, "varIdx"), length) > 0]
+                            soterms <- unique(c(soterms, VariantFiltering:::.ancestorsSO(VariantFiltering::param(x), soterms)))
+                            soterms <- soterms[sapply(graph::nodeData(VariantFiltering::sog(x), soterms, "varIdx"), length) > 0]
                           }
 
                           mask <- rep(TRUE, length(x))
                           if (length(soterms) > 0) {
-                            varidxinsog <- unique(unlist(nodeData(sog(x), soterms, "varIdx"), use.names=FALSE))
+                            varidxinsog <- unique(unlist(graph::nodeData(VariantFiltering::sog(x), soterms, "varIdx"), use.names=FALSE))
                             mask <- rep(FALSE, length(x))
                             mask[varidxinsog] <- TRUE
                           }
