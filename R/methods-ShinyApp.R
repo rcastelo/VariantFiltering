e0caeb306af6fbfca295d7cdd17dc919653c5c54
JoanFernandez <joanfernandez1331@gmail.com>
2018-01-26 11:15:36 +0100

    New onefile app

--- R/methods-ShinyApp.R
+++ R/methods-ShinyApp.R
@@ -1,4 +1,128 @@
-browseVariants <- function(vfResultsObj) {
+browseVariants <- function() {
+  ##
+  ## Build
+  ##
+  
+  ## build construction params
+  mainDataInput <- function(id) {
+    ns <- NS(id)
+    mainPanel(width = 6,
+              h2("Select data source:"),
+              wellPanel(fluidRow(
+                column(
+                  12,
+                  #vcfFilename
+                  fileInput("vcfFilename", strong("Choose vcf:"),
+                            accept = c(".vcf",".vcf.gz",".vcf.bgz")
+                  ),
+                  #pedFilename
+                  fileInput("pedFilename", strong("Choose ped:"),
+                            accept = c(".ped")
+                  ),
+                  HTML("<br>"),
+                  #orgdb
+                  selectInput("orgdb", strong("Select orgdb:"),
+                              VariantFiltering:::.orgdbpkgs()),
+                  HTML("<br>"),
+                  #txdb
+                  selectInput("txdb", strong("Select txdb:"),
+                              VariantFiltering:::.txdbpkgs()),
+                  HTML("<br>"),
+                  #snpdb
+                  selectInput("snpdb", strong("Select snpdb:"),
+                              VariantFiltering:::.snpdbpkgs()),
+                  selectInput("snpdb2","",
+                              VariantFiltering:::.xtrasnpdbpkgs()),
+                  HTML("<br>"),
+                  #otherAnnotation
+                  checkboxGroupInput("otherAnnotations",strong("Select other annotations:"),VariantFiltering:::.otherannotations())
+                  
+                )
+              ),
+              HTML("<br>"),
+              actionButton("returnButton", strong("Build"), class = "btn-primary"),
+              disabled(actionButton("run", strong("Run"), class = "btn-info")),
+              disabled(actionButton("browse", strong("Browse Variants"), class = "btn-success")),
+              disabled(downloadButton("closeSave", strong("Save variants file"), class = "btn-warning"))))
+  }
+  
+  ## validates construction params
+  dataValidation <- function(input,output,session) {
+    flag <- FALSE
+    
+    #vcf
+    if (!is.null(input$vcfFilename)) {
+      vcf <- input$vcfFilename
+      flag <- TRUE
+    } else flag <- FALSE
+    #ped
+    if (!is.null(input$pedFilename)) {
+      ped <- input$pedFilename
+      flag <- TRUE
+    } else flag <- FALSE
+    
+    #orgdb
+    if (!is.null(input$orgdb)) {
+      orgdb <- input$orgdb
+    } else orgdb <- character(0)
+    
+    #txdb
+    if (!is.null(input$txdb)) {
+      txdb <- input$txdb
+    } else txdb <- character(0)
+    
+    #snpdb
+    if (!is.null(input$snpdb)) {
+      snpdb <- input$snpdb
+    } else flag <- FALSE
+    
+    if (!is.null(input$snpdb2)) {
+      snpdb2 <- input$snpdb2
+    } else flag <- FALSE
+    
+    #otherAnnotations
+    if (!is.null(input$otherAnnotations)) {
+      otherAnnotations <- input$otherAnnotations
+    } else otherAnnotations <- character(0)
+    
+    if(flag) {
+      sn <- c(snpdb,snpdb2)
+      withProgress(message = 'Building', value = 0, {
+        incProgress(1, detail = "This may take a while...")
+        result = tryCatch({
+          vfpardev <<- VariantFilteringParam(vcfFilename = vcf$datapath,pedFilename=ped$datapath,
+                                             orgdb=orgdb, txdb=txdb, snpdb=sn, otherAnnotations=otherAnnotations)
+        }, warning = function(w) {
+          #warning-handler-code
+        }, error = function(e) {
+          shinyjs::alert("Select correct file!")
+        })
+      })
+    }
+  }
+  
+  ## renderizes construction progress
+  renderProgress <- function(input, output, session, progress, message) {
+    switch(progress,
+           "1" = {
+             output$progress <- renderUI(HTML("<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='40' 
+                                              aria-valuemin='0' aria-valuemax='100' style='width:33%'></div></div>"))
+           },
+           "2" = {
+             output$progress <- renderUI(HTML("<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='40' 
+                                              aria-valuemin='0' aria-valuemax='100' style='width:66%'></div></div>")) 
+           },
+           "3" = {
+             output$progress <- renderUI(HTML("<div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='40' 
+                                              aria-valuemin='0' aria-valuemax='100' style='width:100%'></div></div>"))
+           })
+    output$info <- renderText(message)
+           }
+  
+  ##
+  ## Browse
+  ##
+  
   ## filtering control of logical values
   logicalPanel <- function(flt,tab) {
     conditionalPanel(condition=paste("input.tsp ==", tab, sep = ""), 
@@ -148,10 +272,10 @@ browseVariants <- function(vfResultsObj) {
   }
   
   ## layout the UI of the controls
-  inputData <- function(id, vfrobj) {
+  inputData <- function(id, vfResultsObj) {
     ns <- NS(id)
     
-    tabs <- lapply(names(annoGroups(vfrobj)),
+    tabs <- lapply(names(annoGroups(vfResultsObj)),
                    function(tabname) tabPanel(tabname,
                                               DT::dataTableOutput(paste0("table", tabname)),
                                               value=tolower(tabname)))
@@ -162,6 +286,7 @@ browseVariants <- function(vfResultsObj) {
       #UI declaration
       column(
         3,
+        h2("Select data to filter:"),
         #Data
         wellPanel(fluidRow(
           column(
@@ -169,12 +294,12 @@ browseVariants <- function(vfResultsObj) {
             selectInput("sort", "Select column to sort by:",levels(sortings(vfResultsObj)[["criterion"]]), selected = as.character(sortings(vfResultsObj)[["criterion"]])),
             radioButtons("order","",choiceNames = list("Decreasing", "Increasing"), choiceValues = list(FALSE, TRUE), selected = sortings(vfResultsObj)[["decreasing"]]),
             generateFiltering(vfResultsObj),
-            actionButton("closeSave", "Save and Close")
+            actionButton("close", "Save and close", class = "btn btn-warning")
           )
         ))
       ),
       column(1, uiOutput("pageFilter")),
-      column(8, mainPanel(do.call(tabsetPanel, tabs)))
+      column(8, mainPanel(h2("Variant Visualization"),do.call(tabsetPanel, tabs)))
     )
   }
   
@@ -256,7 +381,6 @@ browseVariants <- function(vfResultsObj) {
   }
   
   ## modify cutoffs
-  
   cutoffsModification <- function(input, output, session, global) {
     lapply(rownames(filtersMetadata(vfResultsObj)[!is.na(filtersMetadata(vfResultsObj)$AnnoGroup),]), function(flt)
       switch(class(cutoffs(vfResultsObj)[[flt]]),
@@ -333,55 +457,58 @@ browseVariants <- function(vfResultsObj) {
     )
   }
   
-  ## recursive function to generate the tree
-  "generateTree <- function(n,g) {
-  sapply(n,
-  function(x){
-  if(length(unlist(edges(g)[[x]])) == 0) {
-  return(1)
-  }else{
-  generateTree(edges(g)[[x]], g)
-  }
-  }, USE.NAMES = TRUE, simplify = FALSE)
-}"
-  
-  ## generates SOterms tree
-"  generateSOtermsTree <- function(vfResultsObj) {
-    gn <- sog(vfResultsObj,reverse=TRUE)
-    to <- tsort(gn)
+  build <- function(input,output,session) {
+    #Button comprovation
+    if(!exists("vfpardev")) {
+      shinyjs::disable("run")
+      renderProgress(input,output,session,1, "Step 1: Build your vfParam object by selecting the parameters in the left.")
+    } else renderProgress(input,output,session, 2, "Step 2: Run unrelated individuals to build your final object.")
     
-    tree <- generateTree(to[1],gn)
-    nodeD <- nodeData(gn, nodes(gn), 'label')
+    if(!exists("vfResultsObj")) {
+      shinyjs::disable("browse")
+      shinyjs::disable("closeSave")
+    } else renderProgress(input,output,session, 3, "Step 3: Browse the results in our visualitzation app or save the generated object
+                       in your workspace.")
     
-    ## renames the tree
-    rename_items <- function(item){
-      if (is.list(item)){
-        item <- lapply(item,rename_items)
-        names(item) <- unname(nodeD[names(item)])
+    #Buttons events
+    observeEvent(input$returnButton, {
+      dataValidation(input,output,session)
+      if(exists("vfpardev")) {
+        shinyjs::enable("run")
+        renderProgress(input,output,session, 2, "Step 2: Run unrelated individuals to build your final object.")
       }
-      item
-    }
+    })
     
-    tree2<-rename_items(tree)
-    return(tree2)
-  }"
+    observeEvent(input$run, {
+      if(exists("vfpardev")) {
+        withProgress(message = 'Building unrelated individuals', value = 0, {
+          incProgress(1, detail = "This may take a while...")
+          vfResultsObj <<- unrelatedIndividuals(vfpardev) 
+        })
+      }
+      if(exists("vfResultsObj")) {
+        shinyjs::enable("browse")
+        shinyjs::enable("closeSave")
+        renderProgress(input,output,session, 3, "Step 3: Browse the results in our visualitzation app or save the generated object
+                       in your workspace.")
+      }
+      })
+    
+    observeEvent(input$browse, {
+      remove(vfpardev)
+      shinyjs::runjs("location.reload();")
+    })
+    
+    output$closeSave <- downloadHandler(
+      filename = function() {
+        paste("my_data",".rds", sep="")
+      },
+      content = function(file){
+        saveRDS(vfResultsObj, file)
+      })
+    }
   
-  ## build the shiny app
-  app <- list(ui = NULL, server = NULL)
-  
-  app$ui <- fluidPage(theme = shinytheme("simplex"),
-                      shinyjs::useShinyjs(),
-                      tags$head(
-                        tags$style(
-                          HTML(".dataTables_filter, .dataTables_info .shiny-bound-input .shiny-bound-output, .dataTables_length, #page { display: none; }"),
-                          HTML("#order{margin-top:-20px; margin-bottom:30px;} "),
-                          HTML("#closeSave{margin-top:20px;} ")
-                        )
-                      ),
-                      inputData("inputData", vfResultsObj)
-  )
-  
-  app$server <- function(input, output, session) {
+  browse <- function(input,output,session) {
     
     global <- reactiveValues()
     
@@ -414,11 +541,6 @@ browseVariants <- function(vfResultsObj) {
     }
     
     tree2<-rename_items(tree1)
-    #tree2 <- generateSOtermsTree(vfResultsObj)
-    
-    #output$tree <- shinyTree::renderTree({ 
-    #  tree2
-    #})
     
     #Page filter
     observeEvent({input$page; input$tsp}, {
@@ -450,10 +572,59 @@ browseVariants <- function(vfResultsObj) {
       datatable_generation(input,output,session,global)
     })
     
-    #Observe the Save & Close button
-    observeEvent(input$closeSave, {
-      stopApp(vfResultsObj) #Stops the app and returns the generated_gsva object
+    observeEvent(input$close, {
+      stopApp(vfResultsObj)
     })
+    
+  }
+  
+  app <- list(ui = NULL, server = NULL)
+  
+  app$ui <- fluidPage(theme = shinytheme("cerulean"),
+                      shinyjs::useShinyjs(),
+                      tags$head(
+                        tags$style(
+                          HTML(".dataTables_filter, .dataTables_info .shiny-bound-input .shiny-bound-output, .dataTables_length, #page { display: none; }"),
+                          HTML("#order{margin-top:-20px; margin-bottom:30px;} "),
+                          HTML("#closeSave{margin-top:20px;} "),
+                          HTML(".btn-warning { float:right; }")
+                        )
+                      ),
+                      uiOutput("ui")
+                      
+  )
+  
+  app$server <- function(input, output, session) {
+    if(!exists("vfResultsObj")) {
+      output$ui <- renderUI({
+        fluidPage(theme = shinytheme("cerulean"), shinyjs::useShinyjs(),
+                  tags$head(
+                    tags$style(
+                      HTML(".btn-warning { float:right; }")
+                    )),
+                  fluidRow(
+                    conditionalPanel(id= "buildPanel",
+                                     column(6),
+                                     mainDataInput("mainInput"),
+                                     column(3,
+                                            h3("Progress"),
+                                            htmlOutput("progress"),
+                                            textOutput("info"))
+                    )
+                  )
+        )
+      })
+      build(input,output,session)  
+    }
+    else {
+      output$ui <- renderUI({
+        inputData("inputData", vfResultsObj) 
+      })
+      browse(input,output,session)
+    }
   }
   runApp(app)
-}
+ }
+
+browseVariants()
+
